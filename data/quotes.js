91bfd63022dfaee0dd61b433038d915b5066dfc7
kylevedder <kyle.c.vedder@gmail.com>
2016-12-17 17:37:15 -0500

    Fixed the attribution

--- data/quotes.js
+++ data/quotes.js
@@ -1,4 +1,4 @@
-var data = ["\"A good dissertation makes a 'thud' when you drop it.\" - Shriram",
+var data = ["A good dissertation makes a 'thud' when you drop it.",
 "Two isn't enough, you need at least three things in a list.",
 "A recommendation can't be a page, because then, people are like,\n \"What? That's just a page,\" and two pages is too long.\n You need to have a page and a quarter.",
 "You'll figure it out.",
