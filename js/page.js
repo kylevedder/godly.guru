00ff8fe8bb27575f1c577577502d91a49b3c7d0e
Kyle Vedder <kyle.c.vedder@gmail.com>
2018-11-08 15:23:55 -0500

    Added suffle and iterate for better distrubution.

--- js/page.js
+++ js/page.js
@@ -2,12 +2,11 @@ var prevQuoteIndex = -1;
 
 
 function getQuote() {
-   index = parseInt(Math.random() * quotes.length);
-   while (index === prevQuoteIndex) {
-       index = parseInt(Math.random() * quotes.length);  
-   }
-   prevQuoteIndex = index;
-   return quotes[index];
+   ++prevQuoteIndex;
+    if (prevQuoteIndex >= quotes.length) {
+        prevQuoteIndex = 0;   
+    }
+    return quotes[prevQuoteIndex];
 }
 
 function setText() {
@@ -18,4 +17,24 @@ document.getElementById("advice").addEventListener("click", function(){
     setText();
 });
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+quotes = shuffle(quotes);
 setText();
